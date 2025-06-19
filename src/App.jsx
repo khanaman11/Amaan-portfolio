fc96e44400b40ea7fc77ccb63774ce03498a7021
Aman khan <amanullahkhan11595@gmail.com>
2025-06-19 14:16:25 +0530

    next commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Hero from './Components/Hero/Hero'
 import About from './Components/About/About'
 import Services from './Components/Services/Services'
 import MyWork from './Components/MyWork/MyWork'
-import Contact from './Components/Contact/COntact'
+import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer'
 
 function App() {
