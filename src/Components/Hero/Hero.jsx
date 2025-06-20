e73ecc2b985f819a7215d11581ba77d96f3224c8
Aman khan <amanullahkhan11595@gmail.com>
2025-06-20 17:37:22 +0530

    last commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 import profile from "../../assets/my_image.jpg"
 import "./Hero.css"
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import resume from "../../assets/Amanullah updated CV.pdf"
 
 const Hero = () => {
   return (
     <div className='hero' id='home'>
-        <img src={profile} alt=""/>
-        <h2><span className='hero-header'>I'm Amaan Khan,</span> Frontend developer based in Delhi.</h2>
-        <p>I am a frontend developer from Delhi India. with 2 year of experience with multipal  company like. capital technology ,  PTT , Testpan india.</p>
-        <div className='hero-action-btn'>
-            <button className='hero-connect'> <AnchorLink className='anchor-link' offset={50} href='#contact'>Contact me</AnchorLink></button>
-            <button className='hero-resume'>My resume</button>
-        </div>
+      <img src={profile} alt="" />
+      <h2><span className='hero-header'>I'm Amanullah Khan,</span> Frontend developer based in Delhi.</h2>
+      <p>I am a frontend developer from Delhi India. with 2 year of experience with multipal  company like. capital technology ,  PTT , Testpan india.</p>
+      <div className='hero-action-btn'>
+        <button className='hero-connect'> <AnchorLink className='anchor-link' offset={50} href='#contact'>Contact me</AnchorLink></button>
+        <a href={resume} download className="hero-resume">
+          My Resume
+        </a>
+      </div>
     </div>
   )
 }
