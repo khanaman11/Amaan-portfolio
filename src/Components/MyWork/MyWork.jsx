88f34f04ff217777454e4dce820a41b42a288cdc
Aman khan <amanullahkhan11595@gmail.com>
2025-06-20 16:40:04 +0530

    new code

--- src/Components/MyWork/MyWork.jsx
+++ src/Components/MyWork/MyWork.jsx
@@ -3,29 +3,38 @@ import "./MyWork.css"
 import theme_pattern from "../../assets/theme_pattern.svg"
 import mywork_data from '../../assets/mywork_data'
 import arrow_icon from "../../assets/arrow_icon.svg"
+import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const MyWork = () => {
-  return (
-    <div className='mywork' id='work'>
-        <div className="mywork-title">
-            <h3>My latest Work</h3>
-            <img src={theme_pattern} alt="" />
+    return (
+        <div className='mywork' id='work'>
+            <div className="mywork-title">
+                <h3>My latest Work</h3>
+                <img src={theme_pattern} alt="" />
+            </div>
+            <div className="mywork-container">
+                {
+                    mywork_data.map((work, index) => {
+                        return <div key={index} className='work-img-container'>
+                            <img src={work.w_img} alt='work-img' />
+                            <div className="work-btn-cnt">
+                                <a href={work.p_link} className='live-btn' target="_blank" rel="noopener noreferrer">
+                                    Live Work
+                                </a>
+                                <a href={work.G_link} className='github' target="_blank" rel="noopener noreferrer">
+                                    GitHub Code
+                                </a>
+                            </div>
+                        </div>
+                    })
+                }
+            </div>
+            <div className="mywork-showmore">
+                <p>Show More</p>
+                <img src={arrow_icon} alt="" />
+            </div>
         </div>
-        <div className="mywork-container">
-            {
-                mywork_data.map((work , index)=>{
-                    return <div  key={index} className='work-img-container'>
-                        <img src={work.w_img} alt='work-img'/>
-                    </div>
-                })
-            }
-        </div>
-        <div className="mywork-showmore">
-            <p>Show More</p>
-            <img src={arrow_icon} alt="" />
-        </div>
-    </div>
-  )
+    )
 }
 
 export default MyWork
--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,41 +1,53 @@
-import project1_img from '../assets/project_1.svg'
-import project2_img from '../assets/project_2.svg'
-import project3_img from '../assets/project_3.svg'
-import project4_img from '../assets/project_4.svg'
-import project5_img from '../assets/project_5.svg'
-import project6_img from '../assets/project_6.svg'
+import Assessment from '../assets/assessment-img.png'
+import testcenter from '../assets/test-center-img.png'
+import crudOperation from '../assets/crud-operation.png'
+import ecom from '../assets/e-com-img.png'
+import testpan from '../assets/testpan-img.png'
+import BMTC_landingPage from '../assets/bmtc-landing.png'
 
 const mywork_data = [
     {
-        w_no:1,
-        w_name:"Web design",
-        w_img:project1_img
+        w_no: 1,
+        w_name: "Assessment company",
+        w_img: Assessment,
+        p_link: "https://bmtc-html-project.netlify.app/",
+        G_link: "https://github.com/khanaman11/Assessment-company-portal.git"
     },
     {
-        w_no:2,
-        w_name:"Web design",
-        w_img:project2_img
+        w_no: 2,
+        w_name: "Test Center",
+        w_img: testcenter,
+        p_link: "https://test-center-eight.vercel.app/",
+        G_link: "https://github.com/khanaman11/Test-Center-booking-portal.git"
     },
     {
-        w_no:3,
-        w_name:"Web design",
-        w_img:project3_img
+        w_no: 3,
+        w_name: "Crud operation",
+        w_img: crudOperation,
+        p_link: "https://crud-employ-system.vercel.app/",
+        G_link: "https://github.com/khanaman11/crud-employ-system.git"
     },
     {
-        w_no:4,
-        w_name:"Web design",
-        w_img:project4_img
+        w_no: 4,
+        w_name: "E-commerce",
+        w_img: ecom ,
+        p_link: "https://electronic-commerce-website.netlify.app/",
+        G_link: "https://github.com/khanaman11/e-commerce.git"
     },
     {
-        w_no:5,
-        w_name:"Web design",
-        w_img:project5_img
+        w_no: 5,
+        w_name: "Testpan_india",
+        w_img: testpan,
+        p_link: "https://testpan-company-website.netlify.app/",
+        G_link: "https://github.com/khanaman11/Testpan-company-website.git"
     },
     {
-        w_no:6,
-        w_name:"Web design",
-        w_img:project6_img
+        w_no: 6,
+        w_name: "bmtc-landing-page",
+        w_img: BMTC_landingPage,
+        p_link: "https://bmtc-landing-page.vercel.app/",
+        G_link: "https://github.com/khanaman11/BMTC_landingPage.git"
     },
 ]
- 
+
 export default mywork_data;
